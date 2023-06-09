2c7e249338f3026075a8bea5e76a3343265191fe
Shaw Lu <shawlu95@gmail.com>
2023-06-08 18:03:32 -0700

    play sound when egg finishes

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     var elapsed = 0
     var totalTime = 0
     var timer = Timer()
+    var player: AVAudioPlayer!
     
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLabel: UILabel!
     let eggTimes : [String : Int] = ["Soft": 5, "Medium": 7, "Hard": 10]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        totalTime = eggTimes[sender.currentTitle!]!
+        let hardness = sender.currentTitle!
+        titleLabel.text = hardness
+        totalTime = eggTimes[hardness]!
         elapsed = 0
         progressBar.progress = 0.0
         
@@ -37,6 +41,10 @@ class ViewController: UIViewController {
             timer.invalidate()
             titleLabel.text = "Done!"
             progressBar.progress = 1.0
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
         }
     }
 }
